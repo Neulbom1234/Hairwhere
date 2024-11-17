a140d4353bb059819bfd11c2c88ba98c6b7b3a6d
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2024-11-17 23:12:51 +0900

    update

--- back/neulbom/src/main/java/com/example/neulbom/service/PhotoService.java
+++ back/neulbom/src/main/java/com/example/neulbom/service/PhotoService.java
@@ -11,18 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -30,54 +25,50 @@ import lombok.RequiredArgsConstructor;
 public class PhotoService {
 
     private final PhotoRepository photorepository;
-    private final s3Service s3Service;
+    private final NCPStorageService ncpStorageService;
 
     private final int MIN_RANDOM_NUM = 1;
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Transactional
     public Long upload(String name, MultipartFile[] image,
-                                 int likeCount, String hairName, String text, String gender, LocalDateTime created,
-                                 String hairSalon, String hairSalonAddress, String hairLength, String hairColor, User user) {
+                       int likeCount, String hairName, String text, String gender, LocalDateTime created,
+                       String hairSalon, String hairSalonAddress, String hairLength, String hairColor, User user) {
         List<String> imagePaths = new ArrayList<>();
 
-        for( MultipartFile file : image ) {
-            try{ // image생성안함 이 부분만 차이남
-                String imagePath = s3Service.upload(file);
+        for(MultipartFile file : image) {
+            try {
+                String imagePath = ncpStorageService.upload(file);
                 imagePaths.add(imagePath);
             }
             catch (Exception e) {
-                logger.error("Error uploading file to S3", e);
+                logger.error("Error uploading file to NCP", e);
                 throw new RuntimeException("파일 업로드 중 오류가 발생했습니다.", e);
             }
         }
 
         Photo photo = Photo.builder()
-                .userName(name)
-                .photoImagePath(imagePaths)
-                .likeCount(likeCount)
-                .hairName(hairName)
-                .text(text)
-                .gender(gender)
-                .created(created)
-                .hairSalon(hairSalon)
-                .hairSalonAddress(hairSalonAddress)
-                .hairLength(hairLength)
-                .hairColor(hairColor)
-                .user(user)
-                .build();
+            .userName(name)
+            .photoImagePath(imagePaths)
+            .likeCount(likeCount)
+            .hairName(hairName)
+            .text(text)
+            .gender(gender)
+            .created(created)
+            .hairSalon(hairSalon)
+            .hairSalonAddress(hairSalonAddress)
+            .hairLength(hairLength)
+            .hairColor(hairColor)
+            .user(user)
+            .build();
 
         photorepository.save(photo);
-
-        Long id = photo.getId();
-
-        return id;
+        return photo.getId();
     }
 
     @Transactional
     public Page<PhotoResponse> findByHairSalon(String hairSalon,Pageable pageable){
         Page<Photo> photo = photorepository.findByHairSalon(hairSalon,pageable);
-
         return photo.map(PhotoResponse::fromEntity);
     }
 
@@ -100,14 +91,12 @@ public class PhotoService {
     @Transactional
     public Page<PhotoResponse> findByGender(String gender,Pageable pageable) {
         Page<Photo> photo = photorepository.findByGender(gender,pageable);
-
         return photo.map(PhotoResponse::fromEntity);
     }
 
     @Transactional
     public Page<PhotoResponse> findAll(Pageable pageable) {
         Page<Photo> photo = photorepository.findAll(pageable);
-
         return photo.map(PhotoResponse::fromEntity);
     }
 
@@ -116,13 +105,10 @@ public class PhotoService {
         Photo photo = findPhotoById2(photoId);
 
         if(validateUserName(name, photo.getUserName())){
-
             for(String imagePath : photo.getPhotoImagePath()) {
-                s3Service.deleteImageFromS3(imagePath);
+                ncpStorageService.deleteImageFromNCP(imagePath);
             }
-
             photorepository.delete(photo);
-
             return "삭제 완료";
         }
         else{
@@ -133,7 +119,6 @@ public class PhotoService {
     @Transactional
     public Page<PhotoResponse> findByUserName(String userName,Pageable pageable) {
         Page<Photo> photo = photorepository.findByUserName(userName,pageable);
-
         return photo.map(PhotoResponse::fromEntity);
     }
 
@@ -145,18 +130,20 @@ public class PhotoService {
 
     @Transactional
     public void deleteLikeCount(Long photoId) {
-        Photo photo = photorepository.findById(photoId).orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
+        Photo photo = photorepository.findById(photoId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
         photo.decreaseLikeCount();
     }
 
     private PhotoResponse findPhotoById(Long photoId) {
-        Photo photo = photorepository.findById(photoId).orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
-
+        Photo photo = photorepository.findById(photoId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
         return PhotoResponse.fromEntity(photo);
     }
 
     private Photo findPhotoById2(Long photoId) {
-        return photorepository.findById(photoId).orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
+        return photorepository.findById(photoId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사진이 없습니다."));
     }
 
     public void save(Photo photo) {
@@ -164,7 +151,6 @@ public class PhotoService {
     }
 
     public Page<PhotoResponse> findLikedPhotosByUser(List<Like> likes, Pageable pageable) {
-
         if (likes.isEmpty()) {
             logger.error("예외처리에 의한 빈 likes" );
             return Page.empty();
@@ -181,8 +167,8 @@ public class PhotoService {
         }
 
         List<Photo> photos = likes.stream()
-                .map(Like::getPhoto)
-                .collect(Collectors.toList());
+            .map(Like::getPhoto)
+            .collect(Collectors.toList());
 
         int start = (int) pageable.getOffset();  // 페이지 시작 인덱스
         int end = Math.min((start + pageable.getPageSize()), photos.size());
@@ -193,9 +179,8 @@ public class PhotoService {
     }
 
     public Page<PhotoResponse> search(String hairName, String hairLength,String hairColor,
-                              String gender, Pageable pageable){
+                                      String gender, Pageable pageable){
         Page<Photo> photo = photorepository.search(hairName,hairLength,hairColor,gender,pageable);
-
         return photo.map(PhotoResponse::fromEntity);
     }
 
