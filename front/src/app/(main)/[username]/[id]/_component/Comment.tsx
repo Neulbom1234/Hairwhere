1d9a82f29ab5cdf907800db621251b94d57d67ff
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-11-17 18:20:41 +0900

    [feat] 댓글 불러오기 API 연결

--- front/src/app/(main)/[username]/[id]/_component/Comment.tsx
+++ front/src/app/(main)/[username]/[id]/_component/Comment.tsx
@@ -1,21 +1,37 @@
 import { Avatar } from 'antd';
 import style from './comment.module.css';
 import { UserOutlined } from '@ant-design/icons';
+import { Comment as IComment } from '@/model/Comment';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-export default function Comment() {
+dayjs.locale('ko');
+dayjs.extend(relativeTime)
+
+type Props = {
+  comment: IComment
+}
+
+export default function Comment({comment}: Props) {
   return (
-    <div className={style.comment}>
-      <Avatar icon={<UserOutlined/>} className={style.profile} size={42}/>
-      <div className={style.textWrapper}>
-        <div className={style.userName}>산호초</div>
-        <div className={style.text}>
-          우와 너무 예뻐요!!!
-        </div>
-        <div className={style.commentBottom}>
-          <span className={style.time}>6시간 전</span>
-          <span className={style.recomment}>답글 10개 보기</span>
+    <>
+      <div className={style.comment}>
+        <Avatar src={comment.user.profilePath} className={style.profile} size={42}/>
+        <div className={style.textWrapper}>
+          <div className={style.userName}>{comment.user.name}</div>
+          <div className={style.text}>{comment.content}</div>
+          <div className={style.commentBottom}>
+            <span className={style.time}>{dayjs(comment.createdAt).fromNow(true)} 전</span>
+            <span className={style.recomment}>
+              {comment.replies.length === 0 ?
+                "답글 달기"
+                : `답글 ${comment.replies.length}개 보기`
+              }
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </> 
+    )
+    
 }
