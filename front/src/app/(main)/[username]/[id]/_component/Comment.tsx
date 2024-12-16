bed87a244f3387c7835d17045f1b5af3ca7d4672
san9901 <san990190@gmail.com>
2024-12-16 19:35:43 +0900

    [feat] 댓글 삭제 기능 추가

--- front/src/app/(main)/[username]/[id]/_component/Comment.tsx
+++ front/src/app/(main)/[username]/[id]/_component/Comment.tsx
@@ -7,6 +7,8 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { useStore } from '@/store/store';
 import { useRouter } from 'next/navigation';
+import { useSession } from "next-auth/react";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -21,6 +23,8 @@ export default function Comment({comment, id}: Props) {
     setRecomment: state.setRecomment,
     setPhotoId: state.setPhotoId
   }));
+  const queryClient = useQueryClient();
+  const { data: me } = useSession(); 
   const router = useRouter();
 
   const redirectToRecomment = (comment: IComment) => {
@@ -29,6 +33,39 @@ export default function Comment({comment, id}: Props) {
     router.push('/recomment');
   }
 
+  const deleteComment = useMutation({
+    mutationFn: () => {
+      return fetch(`/comment/deleteComment/${comment.id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+    },
+    onMutate() {
+      const queryCache = queryClient.getQueryCache();
+      const queryKeys = queryCache.getAll().map(cache => cache.queryKey);
+      queryKeys.forEach((queryKey) => {
+        if(queryKey[0] === "comments" && queryKey[1] === id) {
+          const value: IComment[] | undefined = queryClient.getQueryData(queryKey);
+          const shallow = value ? [...value] : [];
+          shallow.filter((c) => c.id !== comment.id);
+          queryClient.setQueryData(queryKey, shallow);
+        }
+      })
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({queryKey: ['comments', id]});
+    },
+    onError: (error) => {
+      console.error("Error deleting comment:", error);
+    }
+  })
+
+  const onDeleteComment = () => {
+    deleteComment.mutate();
+  }
 
   return (
     <>
@@ -46,6 +83,11 @@ export default function Comment({comment, id}: Props) {
                   "답글 달기"
                   : `답글 ${comment.replies.length}개 보기`
               }
+            </span>
+            <span className={style.delete} onClick={() => onDeleteComment()}>
+              {comment.user.name === me?.user?.name ?
+                "삭제하기":
+                <></>}
             </span>
           </div>
         </div>
